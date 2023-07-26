bd6f09086cf439061fad528e68361ecfe2ee72fb
Nick <ngreen@v4development.com>
2023-07-26 09:31:09 -0400

    finished building static page

--- src/pages/ManageGroupsPage.vue
+++ src/pages/ManageGroupsPage.vue
@@ -1,7 +1,67 @@
 <template>
-  <div class="row">
-    <div class="col">ManageGroups</div>
-  </div>
+  <q-page class="q-px-xl q-mx-xl">
+    <div class="row q-mt-lg">
+      <div class="col q-mt-lg text-center groups-header">Manage Groups</div>
+    </div>
+
+    <div class="row">
+      <div class="col q-mt-lg text-center groups-text">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce pharetra, elit et volutpat elementum.
+      </div>
+    </div>
+
+    <div class="row q-mt-xl">
+      <div class="col text-center">
+        <q-btn
+          style="width: 265px; height: 60px; background-color: #8AAAE5"
+          class="q-mr-md cancel-image-btn"
+          label="Show All Groups"
+          text-color="white"
+          @click="showAllButtonClick"
+        />
+        <q-btn
+          style="width: 265px; height: 60px; background-color: #8AAAE5"
+          class="q-ml-md save-image-btn"
+          label="Download All Groups"
+          text-color="white"
+          @click="downloadButtonClick"
+        />
+      </div>
+    </div>
+
+    <div class="row q-mt-xl">
+      <div class="col q-pr-md">
+        <q-input v-model="name" label="Name" outlined />
+      </div>
+      <div class="col q-px-md">
+        <q-input v-model="description" label="Description" outlined />
+      </div>
+      <div class="col q-px-md">
+        <q-input v-model="email" label="Email" outlined />
+      </div>
+      <div class="col q-pl-md">
+        <q-input v-model="cmcMembers" label="CMC Members" outlined />
+      </div>
+    </div>
+
+    <div class="row q-mt-xl">
+      <div class="col text-center">
+        <q-btn
+          style="width: 265px; height: 60px; background-color: #8AAAE5"
+          class="q-mr-md cancel-image-btn"
+          label="View Details / Edit"
+          text-color="white"
+          @click="viewDetailsButtonClick"
+        />
+      </div>
+    </div>
+
+    <div class="row q-mt-xl">
+      <div class="col q-mt-lg text-center groups-text">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce pharetra, elit et volutpat elementum.
+      </div>
+    </div>
+  </q-page>
 </template>
 
 <script setup>
@@ -9,6 +69,7 @@
  * Imports
  ****************************/
 // all component imports here
+import { ref } from "vue";
 
 /*****************************
  * Lazy/Async components
@@ -34,6 +95,10 @@
  * Ref/UI Variables
  ***************************/
 // ref/ui variables here
+const name = ref("");
+const description = ref("");
+const email = ref("");
+const cmcMembers = ref("");
 
 /****************************
  * Computed Properties
@@ -54,6 +119,15 @@
  * UI Functions
  ***************************/
 // ui functions here
+function showAllButtonClick() {
+  console.log("show all button click");
+};
+function downloadButtonClick() {
+  console.log("download all button click");
+};
+function viewDetailsButtonClick() {
+  console.log("view details / edit button click");
+};
 
 /****************************
  * View Lifecycle Methods
@@ -61,4 +135,13 @@
 // view lifecycle methods here
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@import "src/css/app.scss";
+
+.groups-header {
+  color: $vims-medium-blue;
+  font-size: 3.75em;
+  font-weight: 900;
+}
+
+</style>
