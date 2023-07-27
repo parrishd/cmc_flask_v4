7254f7b76d4ec101faf0743363f537adda1bd55a
Nick <ngreen@v4development.com>
2023-07-27 17:35:56 -0400

    initial page commit

--- src/pages/UploadWaterQualityPage.vue
+++ src/pages/UploadWaterQualityPage.vue
@@ -1,7 +1,41 @@
 <template>
-  <div class="row">
-    <div class="col">UploadWaterQualityPage</div>
-  </div>
+  <q-page class="q-px-xl q-mx-xl">
+    <div class="row q-mt-lg">
+      <div class="col q-mt-lg text-center water-quality-header">Upload Water Quality</div>
+    </div>
+
+    <div>
+      <div class="col q-mt-lg">
+        There are two different methods for uploading data to the Data Explorer: Single sample event uploads through
+        a data entry form OR multiple sample event uploads by uploading a spreadsheet file. The file for the latter
+        method needs to adhere to a very specific file format, a template for which can be downloaded here.
+        Additional details can be found in the Data Explorer User's Manual.
+      </div>
+      <div class="col q-mt-xl">
+        Choose the type of upload you would like to do today:
+      </div>
+    </div>
+
+    <div class="row q-mt-xl">
+      <div class="col text-center">
+        <q-btn
+          style="width: 300px; height: 60px; background-color: #8AAAE5"
+          class="q-mr-md cancel-image-btn"
+          label="Form"
+          text-color="white"
+          @click="waterQualityFormClick"
+        />
+        <q-btn
+          style="width: 300px; height: 60px; background-color: #8AAAE5"
+          class="q-ml-md save-image-btn"
+          label="Bulk File"
+          text-color="white"
+          @click="waterQualityBulkClick"
+        />
+      </div>
+    </div>
+
+  </q-page>
 </template>
 
 <script setup>
@@ -25,10 +59,13 @@
  ***************************/
 // props defined here
 
+
+
 /****************************
  * Local/'Use' Variables
  ***************************/
 // local/use variables here
+
 
 /****************************
  * Ref/UI Variables
@@ -54,6 +91,12 @@
  * UI Functions
  ***************************/
 // ui functions here
+function waterQualityFormClick() {
+  console.log('water quality form button click');
+}
+function waterQualityBulkClick() {
+  console.log('water quality bulk upload button click');
+}
 
 /****************************
  * View Lifecycle Methods
@@ -61,4 +104,12 @@
 // view lifecycle methods here
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@import "src/css/app.scss";
+
+.water-quality-header {
+  color: $vims-medium-blue;
+  font-size: 3.75em;
+  font-weight: 900;
+}
+</style>
