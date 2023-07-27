d84a2a0f301f1a3f581913be837cea8f368dc028
Nick <ngreen@v4development.com>
2023-07-27 10:05:00 -0400

    initial build commit

--- src/components/ManageStationsTable.vue
+++ src/components/ManageStationsTable.vue
@@ -1,6 +1,35 @@
 <template>
-  <div class="row">
-    <div class="col">ManageStationsTable</div>
+  <div>
+    <q-table
+      flat bordered
+      :rows="rows"
+      :columns="columns"
+      row-key="id"
+      hide-bottom
+    >
+      <template v-slot:body-cell-editAction="props">
+        <q-td :props="props">
+          <q-btn color="primary" @click="editDetails(props.row.id)">
+            <q-icon class="fa-solid fa-pen-to-square" size="14px"/>
+          </q-btn>
+        </q-td>
+      </template>
+
+      <template v-slot:body-cell-isActive="props">
+        <q-td :props="props">
+          <q-btn v-if="props.row.isActive" color="red" @click="deActivateUser(props.row.stationName)" no-caps>
+            <q-icon class="fa-solid fa-ban" size="14px"/>
+            <span class="q-ml-sm">De-Activate User</span>
+          </q-btn>
+
+          <q-btn v-else color="primary" text-color="white" @click="activateUser(props.row.stationName)" no-caps>
+            <q-icon class="fa-solid fa-plus" size="14px"/>
+            <span class="q-ml-sm">Activate User</span>
+          </q-btn>
+        </q-td>
+      </template>
+
+    </q-table>
   </div>
 </template>
 
@@ -24,6 +53,49 @@
  * Component Props
  ***************************/
 // props defined here
+const columns = [
+  { name: 'stationName', label: 'Name', field: 'stationName', align: 'left' },
+  { name: 'groupNames', label: 'Group Name(s)', field: 'groupNames', align: 'left' },
+  { name: 'latitude', label: 'Latitude', field: 'latitude', align: 'left' },
+  { name: 'longitude', label: 'Longitude', field: 'longitude', align: 'left' },
+  { name: 'editAction', field: 'edit', align: 'left' },
+  { name: 'isActive', field: 'isActive', align: 'left' },
+]
+
+const rows = [
+  {
+    stationName: '35',
+    groupNames: 'Virginia Institute of Marine Science',
+    latitude: '37.52',
+    longitude: '-77.47',
+    editAction: '',
+    isActive: false
+  },
+  {
+    stationName: '35',
+    groupNames: 'Virginia Institute of Marine Science',
+    latitude: '37.52',
+    longitude: '-77.47',
+    editAction: '',
+    isActive: true
+  },
+  {
+    stationName: '35',
+    groupNames: 'Virginia Institute of Marine Science',
+    latitude: '37.52',
+    longitude: '-77.47',
+    editAction: '',
+    isActive: true
+  },
+  {
+    stationName: '35',
+    groupNames: 'Virginia Institute of Marine Science',
+    latitude: '37.52',
+    longitude: '-77.47',
+    editAction: '',
+    isActive: false
+  },
+]
 
 /****************************
  * Local/'Use' Variables
@@ -54,6 +126,15 @@
  * UI Functions
  ***************************/
 // ui functions here
+const editDetails = (stationName) => {
+  console.log(`edit details for station: ${stationName}`);
+}
+const deActivateUser = (id) => {
+  console.log(`De-Activate station: ${stationName}`);
+}
+const activateUser = (id) => {
+  console.log(`Activate station: ${stationName}`);
+}
 
 /****************************
  * View Lifecycle Methods
--- src/pages/ManageStationsPage.vue
+++ src/pages/ManageStationsPage.vue
@@ -1,7 +1,52 @@
 <template>
-  <div class="row">
-    <div class="col">ManageStationsPage</div>
-  </div>
+  <q-page class="q-px-xl q-mx-xl">
+    <div class="row q-mt-lg">
+      <div class="col q-mt-lg text-center stations-header">Manage Stations</div>
+    </div>
+
+    <div class="row">
+      <div class="col q-mt-lg text-center stations-text">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce pharetra, elit et volutpat elementum.
+      </div>
+    </div>
+
+    <div class="row q-mt-xl">
+      <div class="col text-center">
+        <q-btn
+          style="width: 265px; height: 60px; background-color: #8AAAE5"
+          class="q-mr-md cancel-image-btn"
+          label="Add New Station"
+          text-color="white"
+          @click="newStationButtonClick"
+        />
+        <q-btn
+          style="width: 265px; height: 60px; background-color: #8AAAE5"
+          class="q-ml-md save-image-btn"
+          label="Upload Bulk Stations"
+          text-color="white"
+          @click="uploadBulkButtonClick"
+        />
+        <q-btn
+          style="width: 265px; height: 60px; background-color: #8AAAE5"
+          class="q-ml-md save-image-btn"
+          label="Download Stations"
+          text-color="white"
+          @click="downloadButtonClick"
+        />
+      </div>
+    </div>
+
+    <div class="row q-mt-xl q-mx-xl q-px-xl">
+      <div class="col q-pr-md">
+        <q-input v-model="searchQuery" placeholder="Search Stations in Table" outlined />
+      </div>
+    </div>
+
+    <div class="row q-mt-xl q-mx-xl q-px-xl">
+      <ManageStationsTable />
+    </div>
+
+  </q-page>
 </template>
 
 <script setup>
@@ -9,6 +54,7 @@
  * Imports
  ****************************/
 // all component imports here
+import ManageStationsTable from "components/ManageStationsTable.vue";
 
 /*****************************
  * Lazy/Async components
@@ -54,6 +100,15 @@
  * UI Functions
  ***************************/
 // ui functions here
+function newStationButtonClick() {
+  console.log("new station button click");
+}
+function uploadBulkButtonClick() {
+  console.log("upload bulk stations button click");
+}
+function downloadButtonClick() {
+  console.log("download stations button click");
+}
 
 /****************************
  * View Lifecycle Methods
@@ -61,4 +116,12 @@
 // view lifecycle methods here
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@import "src/css/app.scss";
+
+.stations-header {
+  color: $vims-medium-blue;
+  font-size: 3.75em;
+  font-weight: 900;
+}
+</style>
