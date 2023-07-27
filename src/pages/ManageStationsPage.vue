f5f6435d94c9053917f1452ca4a37aae3100b061
Nick <ngreen@v4development.com>
2023-07-27 10:04:14 -0400

    fixed dialog box bug, updated button text, updated variable naming

---
+++ src/pages/ManageStationsPage.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="row">
+    <div class="col">ManageStationsPage</div>
+  </div>
+</template>
+
+<script setup>
+/*****************************
+ * Imports
+ ****************************/
+// all component imports here
+
+/*****************************
+ * Lazy/Async components
+ ****************************/
+// lazy loaded components/imports here
+
+/****************************
+ * Emit Definitions
+ ***************************/
+// emits defined here
+
+/****************************
+ * Component Props
+ ***************************/
+// props defined here
+
+/****************************
+ * Local/'Use' Variables
+ ***************************/
+// local/use variables here
+
+/****************************
+ * Ref/UI Variables
+ ***************************/
+// ref/ui variables here
+
+/****************************
+ * Computed Properties
+ ***************************/
+// computed properties here
+
+/***************************
+ * Watched properties
+ **************************/
+// watched properties here
+
+/****************************
+ * Exposed/Interface Functions
+ ***************************/
+// exposed/interface functions here
+
+/****************************
+ * UI Functions
+ ***************************/
+// ui functions here
+
+/****************************
+ * View Lifecycle Methods
+ ***************************/
+// view lifecycle methods here
+</script>
+
+<style lang="scss" scoped></style>
